2eb32698fe8e5f5ab52061a1cb4c2a5e8755dcb3
julsmartinez <juliannamrtnz11@gmail.com>
2024-07-05 03:34:29 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'  ;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var backtotop = document.getElementById("backtotop");
+
+    // When the button is clicked, scroll to the top of the page
+    backtotop.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // Smooth scrolling animation
+        });
+    });
+});
+
