bb1e36b4c55a5f2b4b43196c254c651974e15e6d
julsmartinez <juliannamrtnz11@gmail.com>
2024-07-11 01:03:45 +0800

    3rd

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+$(document).ready(function(){
+    $('.carousel').slick({
+    slidesToShow: 3  ,
+    dots:true,
+    centerMode: true,
+    });
+  });
+  
+  // Slick version 1.5.8s
